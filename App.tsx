dfd9df419214fa183c14ddde99dab7bb4572b4eb
SINTEK Tecnologia <sintech.tec@gmail.com>
2026-01-08 19:36:57 -0300

    chore: Update version to v1.4.0

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-// Deploy v1.3.1 - 2025-12-30T18:45
+// Deploy v1.4.0 - 2026-01-08T19:40
 import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
